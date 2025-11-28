675913c30b24aa5b945ec44055b3cc55b83ded08
Mikhail Radionov <radionov.mik@gmail.com>
2025-11-28 21:02:53 +0100

    Improve error formatting: better handling of invalid credentials messages

--- iOS/LoveConnection/LoveConnection/Utilities/ErrorFormatter.swift
+++ iOS/LoveConnection/LoveConnection/Utilities/ErrorFormatter.swift
@@ -34,7 +34,7 @@ struct ErrorFormatter {
             return "This information is already in use"
         }
 
-        if errorString.contains("invalid credentials") || 
+        if errorString.contains("invalid credentials") ||
            errorString.contains("unauthorized") ||
            errorString.lowercased().contains("invalid email or password") ||
            errorString.lowercased().contains("invalid email") ||
