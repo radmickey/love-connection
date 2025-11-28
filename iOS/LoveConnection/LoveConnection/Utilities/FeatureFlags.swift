e9a93f298f156ed936852a3cd43a9f94c4f2b08e
Mikhail Radionov <radionov.mik@gmail.com>
2025-11-28 21:43:31 +0100

    Fix FeatureFlags: move FeatureFlagsResponse to same file, remove duplicate

--- iOS/LoveConnection/LoveConnection/Utilities/FeatureFlags.swift
+++ iOS/LoveConnection/LoveConnection/Utilities/FeatureFlags.swift
@@ -4,7 +4,7 @@ import Combine
 struct FeatureFlagsResponse: Codable {
     let enableEmailPasswordAuth: Bool
     let enableAppleSignIn: Bool
-    
+
     enum CodingKeys: String, CodingKey {
         case enableEmailPasswordAuth = "enable_email_password_auth"
         case enableAppleSignIn = "enable_apple_sign_in"
