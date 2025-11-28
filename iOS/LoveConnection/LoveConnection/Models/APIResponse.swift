cdfa9372ff05f3c9eab07495dcf3775e7a13758c
Mikhail Radionov <radionov.mik@gmail.com>
2025-11-28 20:53:55 +0100

    Fix AuthResponse JSON mapping: add CodingKeys for refresh_token

--- iOS/LoveConnection/LoveConnection/Models/APIResponse.swift
+++ iOS/LoveConnection/LoveConnection/Models/APIResponse.swift
@@ -12,6 +12,12 @@ struct AuthResponse: Codable {
     let token: String
     let refreshToken: String?
     let user: User
+    
+    enum CodingKeys: String, CodingKey {
+        case token
+        case refreshToken = "refresh_token"
+        case user
+    }
 }
 
 struct ErrorResponse: Codable {
